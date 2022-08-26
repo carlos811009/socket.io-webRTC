46821c226bc7e8327ce507a92d011be0734aebb7
carlos811009 <79139082+carlos811009@users.noreply.github.com>
2022-08-26 15:35:13 +0800

    Delete socket.io-webRTC/client directory

--- socket.io-webRTC/client/src/app/oauth.ts
+++
@@ -1,8 +0,0 @@
-export const OAuthSettings = {
-  appId: 'fc252083-0d0a-40cd-8bb8-2d7d0c4f16e4',
-  redirectUri: 'https://b8e3-220-136-106-190.ngrok.io',
-  scopes: ['user.read', 'mailboxsettings.read', 'calendars.readwrite'],
-  tenantId: '',
-  clientId: '',
-  clientSecret: ''
-};
